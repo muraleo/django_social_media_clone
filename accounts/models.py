663afe5a7e2f5780416d5f9886e207645dd8cc30
muraleo <muraleo80923@gmail.com>
2018-11-09 09:44:47 -0500

    add views to User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User, auth.models.PermissionsMaxin):
+    def __str__(self):
+        return "@{}".format(self.username)
